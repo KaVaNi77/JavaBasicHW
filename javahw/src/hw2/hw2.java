ad03850b5598405db0ff980a3b98cf04ccaa153a
Valentina N <dkvalentina_n@yahoo.com>
2025-02-10 20:24:40 +0100

    hw2

---
+++ javahw/src/hw2/hw2.java
@@ -0,0 +1,40 @@
+package hw2;
+
+public class hw2 {
+    public static void main(String[] args) {
+
+        String order = "Order No ";
+        byte orderNumber1 = 1;
+        System.out.print(order + orderNumber1 + ".");
+
+        String client = "Client: ";
+        String clientNameAlice = "Alice";
+        System.out.println(client + clientNameAlice + ".");
+
+        String product = "Product: ";
+        String productNameSmartphone = "smartphone";
+        System.out.println(product + productNameSmartphone + ".");
+
+        String price = "Price EUR ";
+        float priceSmartphone = 305.99f;
+        System.out.println(price + priceSmartphone + ".");
+
+        String adress = "Adress:";
+        String adressAlice = "Moon Street, 10";
+        System.out.println(adress + adressAlice + ".");
+
+        int orderNumber2 = 2;
+        System.out.println("Order No" + orderNumber2 + "");
+        String clientNameTom = "Tom";
+        System.out.println("Client: " + clientNameTom + ".");
+        String productNameLaptop = "laptop";
+        System.out.println("Product:" + productNameLaptop + ".");
+        float priceLaptop = 570.95f;
+        System.out.println("Price EUR" + priceLaptop + ".");
+        String adressTom = "Terra Street, 17";
+        System.out.println("Adress" + adressTom);
+
+
+        }
+    }
+
